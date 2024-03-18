99b11dd60730d9a03be3832173dec261eba9fa73
abrar-nasim <42700964+abrar-nasim@users.noreply.github.com>
2024-03-18 15:35:52 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const studentID = 3456;
+let studentName = "Amit";
+var studentCIty = "Noida"; // not recommended to use var due to scope issues
+studentPassword = "Algebra123";
+console.table([studentID, studentCIty, studentName, studentPassword]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"sue strict"; // treat all js codes aas newer version
+
+// alert(3+3) //we are using node not browser, here it will give error//
+//number
+//bigint
+//boolean
+//null =>standalone value
+//undefined
+//symbol
+//object
